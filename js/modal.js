5261cab625278949612d4ec06d88caf22f4a91c4
Matthieu Mary <matthieu.mary2@gmail.com>
2022-11-28 11:26:40 +0100

    delete useless script in index.html

--- js/modal.js
+++ js/modal.js
@@ -10,7 +10,7 @@ const validatedFormCloseBtn = document.querySelector(".close-btn");
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += " responsive" ;
   } else {
     x.className = "topnav";
   }
